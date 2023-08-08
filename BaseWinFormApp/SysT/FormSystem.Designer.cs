c95f627ff0a7a67c99181ddf0565a389057814c8
NewPanForMe <50812368+NewPanForMe@users.noreply.github.com>
2023-08-08 10:37:29 +0800

    设备信息

--- BaseWinFormApp/SysT/FormSystem.Designer.cs
+++ BaseWinFormApp/SysT/FormSystem.Designer.cs
@@ -29,96 +29,165 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
-            this.UserNameLabel = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.Is64SysLabel = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.is64BitTb = new System.Windows.Forms.TextBox();
+            this.osVersionTb = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cpuCoresTb = new System.Windows.Forms.TextBox();
+            this.cpuEnableCoresTb = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CPUTb = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.VersionLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "当前系统登录名：";
-            // 
-            // UserNameLabel
-            // 
-            this.UserNameLabel.AutoSize = true;
-            this.UserNameLabel.Location = new System.Drawing.Point(119, 9);
-            this.UserNameLabel.Name = "UserNameLabel";
-            this.UserNameLabel.Size = new System.Drawing.Size(0, 12);
-            this.UserNameLabel.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 33);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 12);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "64位系统：";
-            // 
-            // Is64SysLabel
-            // 
-            this.Is64SysLabel.AutoSize = true;
-            this.Is64SysLabel.Location = new System.Drawing.Point(119, 33);
-            this.Is64SysLabel.Name = "Is64SysLabel";
-            this.Is64SysLabel.Size = new System.Drawing.Size(0, 12);
-            this.Is64SysLabel.TabIndex = 3;
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "64位系统";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "当前系统版本";
+            // 
+            // is64BitTb
+            // 
+            this.is64BitTb.Location = new System.Drawing.Point(89, 14);
+            this.is64BitTb.Name = "is64BitTb";
+            this.is64BitTb.Size = new System.Drawing.Size(292, 21);
+            this.is64BitTb.TabIndex = 3;
+            // 
+            // osVersionTb
+            // 
+            this.osVersionTb.Location = new System.Drawing.Point(89, 41);
+            this.osVersionTb.Name = "osVersionTb";
+            this.osVersionTb.Size = new System.Drawing.Size(292, 21);
+            this.osVersionTb.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.osVersionTb);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.is64BitTb);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(387, 96);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "系统信息";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cpuCoresTb);
+            this.groupBox2.Controls.Add(this.cpuEnableCoresTb);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.CPUTb);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(405, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(383, 96);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "CPU信息";
+            // 
+            // cpuCoresTb
+            // 
+            this.cpuCoresTb.Location = new System.Drawing.Point(83, 41);
+            this.cpuCoresTb.Name = "cpuCoresTb";
+            this.cpuCoresTb.Size = new System.Drawing.Size(294, 21);
+            this.cpuCoresTb.TabIndex = 9;
+            // 
+            // cpuEnableCoresTb
+            // 
+            this.cpuEnableCoresTb.Location = new System.Drawing.Point(83, 68);
+            this.cpuEnableCoresTb.Name = "cpuEnableCoresTb";
+            this.cpuEnableCoresTb.Size = new System.Drawing.Size(294, 21);
+            this.cpuEnableCoresTb.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "CPU启用核心";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "CPU核心";
+            // 
+            // CPUTb
+            // 
+            this.CPUTb.Location = new System.Drawing.Point(83, 14);
+            this.CPUTb.Name = "CPUTb";
+            this.CPUTb.Size = new System.Drawing.Size(294, 21);
+            this.CPUTb.TabIndex = 5;
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 56);
+            this.label3.Location = new System.Drawing.Point(6, 17);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 12);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Version：";
-            // 
-            // VersionLabel
-            // 
-            this.VersionLabel.AutoSize = true;
-            this.VersionLabel.Location = new System.Drawing.Point(119, 56);
-            this.VersionLabel.Name = "VersionLabel";
-            this.VersionLabel.Size = new System.Drawing.Size(0, 12);
-            this.VersionLabel.TabIndex = 5;
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "CPU";
             // 
             // FormSystem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.VersionLabel);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.Is64SysLabel);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.UserNameLabel);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "FormSystem";
             this.Text = "FormSystem";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label UserNameLabel;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label Is64SysLabel;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox is64BitTb;
+        private System.Windows.Forms.TextBox osVersionTb;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label VersionLabel;
+        private System.Windows.Forms.TextBox CPUTb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox cpuCoresTb;
+        private System.Windows.Forms.TextBox cpuEnableCoresTb;
     }
 }
--- BaseWinFormApp/SysT/FormSystem.cs
+++ BaseWinFormApp/SysT/FormSystem.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Management;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,30 +15,100 @@ namespace SysT
 {
     public partial class FormSystem : Form
     {
-        private readonly ManagementClass _management = new ManagementClass();
+        private Dictionary<string,string > _lDictionary = new Dictionary<string,string>();
+
+
+        /// <summary>
+        /// cpu信息
+        /// </summary>
+        private const string CpuPath = "Win32_Processor";
+
+        /// <summary>
+        /// 磁盘信息
+        /// </summary>
+        private const string DiskPath = "Win32_DiskDrive";// Win32_PhysicalMedia
 
         public FormSystem()
         {
             InitializeComponent();
-            InitBaseInfo();
+            Init();//基础信息加载
+            ReadOnly();//只读加载
+            //tick启动
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+        private void Init()
+        {
+            InitSystemInfo();
+            GetCpu();
+            GetDisk();
+        }
+
+        private void ReadOnly()
+        {
+            is64BitTb.ReadOnly = true;
+            osVersionTb.ReadOnly = true;
+            CPUTb.ReadOnly = true;
+            cpuCoresTb.ReadOnly= true;
+            cpuEnableCoresTb.ReadOnly = true;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            InitBaseInfo();
         }
 
 
-        private void InitBaseInfo()
+        private void InitSystemInfo()
         {
-            this.Is64SysLabel.Text = Environment.Is64BitOperatingSystem ? "是" : "否";
-            this.UserNameLabel.Text = Environment.UserName;
-            this.VersionLabel.Text = Environment.Version.ToString();
-
-
+            is64BitTb.Text = Environment.Is64BitOperatingSystem ? "是" : "否";
+            osVersionTb.Text = RuntimeInformation.OSDescription;
 
         }
+        /// <summary>
+        /// 获得CPU信息
+        /// </summary>
+        private void GetCpu()
+        {
+            var moc = new ManagementClass(CpuPath).GetInstances();
+            foreach (var mo in moc)
+            {
+                foreach (var item in mo.Properties)
+                {
+                    if (item.Name == "Name")
+                    {
+                        CPUTb.Text = item.Value.ToString();
+                    }
+                    if (item.Name == "NumberOfCores")
+                    {
+                        cpuCoresTb.Text = item.Value.ToString();
+                    }
+                    if (item.Name == "NumberOfEnabledCore")
+                    {
+                        cpuEnableCoresTb.Text = item.Value.ToString();
+                    }
+                }
+            }
+        }
 
-
+        /// <summary>
+        /// 获取磁盘信息
+        /// </summary>
+        private void GetDisk()
+        {
+            
+            var moc = new ManagementClass(DiskPath).GetInstances();
+            foreach (var mo in moc)
+            {
+                foreach (var item in mo.Properties)
+                {
+                    if (!_lDictionary.ContainsKey(item.Name))
+                    {
+                        _lDictionary.Add(item.Name, item.Value == null ? "" : item.Value.ToString());
+                    }
+                }
+            }
+        }
     }
+
+ 
 }
